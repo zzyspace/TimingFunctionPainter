5d52ede1ee16e4dca7308d745274007460be1515
张志延 <551854173@qq.com>
2016-10-13 15:16:35 +0800

    Initial Commit

---
+++ TimingFunctionPainter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimingFunctionPainter
+//
+//  Created by 张志延 on 16/10/13.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
